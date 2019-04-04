5eb3cc37614fff3815b65a10744f79921d310e0c
Goncalo <g.neto13@hotmail.com>
2019-04-04 11:24:21 -0300

    classes criadas goncalo#1

---
+++ Operacao.java
@@ -0,0 +1,26 @@
+
+public class Operacao {
+
+	int tipo;
+	float valor;
+
+	public void executar() {
+
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+}
